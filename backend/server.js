7de387c8aef1fb5ce4e0ee8b62d4c1c15be39fc2
Bryan Eriksson <bryaneriksson@gmail.com>
2024-09-07 16:41:59 -0700

    Add get, post, and delete methods

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,54 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import Product from "./models/product.model.js";
 
 dotenv.config();
 
 const app = express();
 
-app.get("/products", (req, res) => {});
+app.use(express.json());
+
+app.get("/api/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json({ success: true, data: products });
+  } catch (error) {
+    console.log("Error in fetching products: ", error.message);
+    res.status(500).json({ success: false, message: "Internal server error" });
+  }
+}); 
+
+app.post("/api/products", async (req, res) => {
+  const product = req.body; // user will send this data
+
+  if(!product.name || !product.price || !product.image) {
+    return res.status(422).json({ success: false, message: "Please add all the fields" });
+  }
+
+  const newProduct = new Product(product);
+
+  try {
+    await newProduct.save();
+    res.status(201).json({ success: true, message: "Product added successfully", data: newProduct });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Internal server error" });
+  }
+
+});
+
+app.delete("/api/products/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    await Product.findByIdAndDelete(id);
+    res.status(200).json({ success: true, message: "Product deleted successfully" });
+  } catch (error) {
+    console.log("Error in deleting product: ", error.message);
+    res.status(404).json({ success: false, message: "Product not found" });
+  }
+})
 
 app.listen(5000, () => {
-    connectDB();
-    console.log("Server started at http://localhost:5000");
+  connectDB();
+  console.log("Server started at http://localhost:5000");
 })
