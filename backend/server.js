8db8519a92f2d61767075d09500378c70a36e9da
Bryan Eriksson <bryaneriksson@gmail.com>
2024-09-07 17:52:35 -0700

    Add vite

--- backend/server.js
+++ backend/server.js
@@ -9,10 +9,11 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
+const PORT = process.env.PORT || 5000;
 
 app.use("/api/products", productRoutes);
 
 app.listen(5000, () => {
   connectDB();
-  console.log("Server started at http://localhost:5000");
+  console.log("Server started at http://localhost:" + PORT);
 });
